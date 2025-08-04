6cbbb0ee0aa344a21f3539c7d1cc87a61a97f52e
Simplechris12 <cristofercastillo12102000@gmai.com>
2025-08-03 22:14:19 -0400

    agregados el HTTPRequest, el HTTPResponse y creación el URI con la API de SWAPI, así como un ejemplo de una de las películas para ver si funciona el consumo

--- src/ConsultaPelicula.java
+++ src/ConsultaPelicula.java
@@ -7,22 +7,23 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaPelicula {
-
-    Pelicula buscaPelicula(int numeroDePelicula) {
+    public Pelicula buscaPelicula(int numeroDePelicula) {
         URI direccion = URI.create("https://swapi.py4e.com/api/films/" + numeroDePelicula + "/");
 
+        // Crear un cliente y solicitud HTTP
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(direccion)
                     .build();
 
-        HttpResponse<String> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
-        return new Gson().fromJson(response.body(), Pelicula.class);
+        // Enviar la solicitud y procesar la respuesta
+        try {
+            HttpResponse<String> response = null;
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Pelicula.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontré esa película.");
+        }
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
         ConsultaPelicula consulta = new ConsultaPelicula();
-        Pelicula pelicula = consulta.buscaPelicula(9);
-        System.out.println(pelicula);
-
+        System.out.println("Bienvenido a la aplicación de consulta de películas");
+        System.out.println("Escriba el número de la película de Star Wars que desea consultar (1-6):");
+        try{
+            var numeroDePelicula = Integer.valueOf(lectura.nextLine());
+            Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
+            System.out.println(pelicula);
+        }catch (NumberFormatException e){
+            System.out.println("Número inválido. Por favor, ingrese un número entre 1 y 6." + e.getMessage());
+        }
+        catch (RuntimeException e ){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicación");
+        }
     }
 }
